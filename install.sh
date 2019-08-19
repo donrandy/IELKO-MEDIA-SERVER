79a8524d7983c8cdddd4d9a98ca31b0f2d3df0eb
donrandy <donrandy@gmail.com>
2019-08-19 17:47:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,25 +2,22 @@
 sudo add-apt-repository ppa:mc3man/trusty-media -y
 sudo apt-get update -y
 sudo apt-get install livestreamer build-essential libpcre3 libpcre3-dev libssl-dev  libpcre3 git  software-properties-common php7.0-cli php7.0-curl php7.0-dev php7.0-fpm php7.0-gd php7.0-mysql php7.0-mcrypt php7.0-opcache php-mbstring php7.0-mbstring php7.0-sybase libsybdb5 php-gettext -y
-#sudo apt-get install ffmpeg  -y
+sudo apt-get install ffmpeg  -y
 mkdir ~/working
 mkdir ~/working/IELKO
 mkdir ~/working/nginx-rtmp-module
 mkdir ~/working/ngx_devel_kit
 mkdir ~/working/set-misc-nginx-module
 mkdir ~/working/nginx
-mkdir ~/working/nginx-hmac-secure-link
-git clone https://github.com/upggr/IELKO-MEDIA-SERVER.git ~/working/IELKO
-git clone https://github.com/sergey-dryabzhinsky/nginx-rtmp-module.git ~/working/nginx-rtmp-module
+git clone https://github.com/donrandy/IELKO-MEDIA-SERVER.git ~/working/IELKO
+git clone https://github.com/arut/nginx-rtmp-module.git ~/working/nginx-rtmp-module
 git clone https://github.com/openresty/set-misc-nginx-module.git ~/working/set-misc-nginx-module
 git clone https://github.com/simpl/ngx_devel_kit.git ~/working/ngx_devel_kit
-git clone https://github.com/nginx-modules/nginx-hmac-secure-link.git ~/working/nginx-hmac-secure-link
 wget http://nginx.org/download/nginx-1.13.6.tar.gz -P ~/working
 tar -xf ~/working/nginx-1.13.6.tar.gz -C ~/working/nginx --strip-components=1
 rm ~/working/nginx-1.13.6.tar.gz
 cd ~/working/nginx
-#./configure --with-http_ssl_module --add-module=../nginx-rtmp-module --add-module=../ngx_devel_kit --add-module=../set-misc-nginx-module --add-module=../nginx-hmac-secure-link
-./configure --with-http_ssl_module --add-module=../nginx-rtmp-module --add-module=../ngx_devel_kit --add-module=../set-misc-nginx-module
+./configure --with-http_ssl_module --add-module=../nginx-rtmp-module --add-module=../nginx-rtmp-module --with-http_secure_link_module --add-module=../ngx_devel_kit --add-module=../set-misc-nginx-module
 make -j 2
 sudo make install
 cp ~/working/IELKO/conf/nginx.conf /usr/local/nginx/conf/nginx.conf
@@ -39,7 +36,7 @@ cp ~/working/IELKO/www/ielko-media-server.css /usr/local/nginx/html/ielko-media-
 cp ~/working/IELKO/www/stream.xml /usr/local/nginx/html/stream.xml
 cp ~/working/IELKO/www/testing.png /usr/local/nginx/html/testing.png
 cp ~/working/IELKO/www/favicon.ico /usr/local/nginx/html/favicon.ico
-git clone https://github.com/upggr/ielko-video-player /usr/local/nginx/html/player
+git clone https://github.com/donrandy/ielko-video-player /usr/local/nginx/html/player
 ip=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//' )
 sed -i -- 's/replaceip/'"$ip"'/g' /usr/local/nginx/html/stream.xml
 sed -i -- 's/replaceip/'"$ip"'/g' /usr/local/nginx/html/index.php
