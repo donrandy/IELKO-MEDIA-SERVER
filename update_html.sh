3df97767b2af906af2b4e1131cd925c6d4312e37
donrandy <donrandy@gmail.com>
2019-08-19 17:39:22 +0200

    Update update_html.sh

--- update_html.sh
+++ update_html.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 mkdir ~/working
 mkdir ~/working/IELKO
-git clone https://github.com/upggr/IELKO-MEDIA-SERVER.git ~/working/IELKO
+git clone https://github.com/donrandy/IELKO-MEDIA-SERVER.git ~/working/IELKO
 rm -rf /usr/local/nginx/html/*
 cp ~/working/IELKO/www/index.php /usr/local/nginx/html/index.php
 cp ~/working/IELKO/www/ielko-media-server.css /usr/local/nginx/html/ielko-media-server.css
 cp ~/working/IELKO/www/stream.xml /usr/local/nginx/html/stream.xml
 cp ~/working/IELKO/www/testing.png /usr/local/nginx/html/testing.png
 cp ~/working/IELKO/www/favicon.ico /usr/local/nginx/html/favicon.ico
-git clone https://github.com/upggr/ielko-video-player /usr/local/nginx/html/player
+git clone https://github.com/donrandy/ielko-video-player /usr/local/nginx/html/player
 
 ip=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//' )
 sed -i -- 's/replaceip/'"$ip"'/g' /usr/local/nginx/html/stream.xml
