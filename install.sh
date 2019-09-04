e30b73c3d4b8d2f6fc3496fec86b17186c2eaea3
donrandy <donrandy@gmail.com>
2019-09-04 20:15:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,6 +34,7 @@ rm /usr/local/nginx/html/*
 cp ~/working/IELKO/www/index.php /usr/local/nginx/html/index.php
 cp ~/working/IELKO/www/ielko-media-server.css /usr/local/nginx/html/ielko-media-server.css
 cp ~/working/IELKO/www/stream.xml /usr/local/nginx/html/stream.xml
+cp ~/working/IELKO/www/stat.xsl /usr/local/nginx/html/stat.xsl
 cp ~/working/IELKO/www/testing.png /usr/local/nginx/html/testing.png
 cp ~/working/IELKO/www/favicon.ico /usr/local/nginx/html/favicon.ico
 git clone https://github.com/donrandy/ielko-video-player /usr/local/nginx/html/player
